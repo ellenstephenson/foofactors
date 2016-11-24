d8648c39bb93be7f4ec4374662e68fb1145a592c
Ellen Stephenson <ellen@psych.ubc.ca>
2016-11-24 10:03:21 -0800

    add roxygen comment

--- R/fbind.R
+++ R/fbind.R
@@ -1,12 +1,15 @@
-# library(devtools)
-# use_git()
-
-# There's a problem when you use c() on 2 factors. Let's write a function to fix that.
-
-# a <- factor(c("factors", "don't", "mix", "well"))
-# b <- factor(c("force", "to", "integer", "sucks"))
-# c(a,b)
-
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
 	factor(c(as.character(a), as.character(b)))
 }
