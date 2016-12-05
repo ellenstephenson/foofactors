9744c4459d2a0c39de88298da3ecd8f9fb7c58c6
Ellen Stephenson <ellen@psych.ubc.ca>
2016-12-05 12:50:24 -0800

    add length levels description

--- R/levels_length.R
+++ R/levels_length.R
@@ -1,3 +1,16 @@
+#' Check the length of the factor against the number of levels
+#'
+#'This function detects factors that should be character because # unique values = length
+#'This function does not convert these problematic factors to character.
+#'
+#' @param x factor
+#'
+#' @return logical
+#' @export
+#' @examples
+#' freq_out(iris$Species)
+#' levels_length(iris$Species)
+
 levels_length <- function(x) {
 	length(x) == dplyr::n_distinct(levels(x))
 }
